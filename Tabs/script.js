62caf5592622c6f8a95aee8bf8009738b9485da9
Zane <subitbravepoudel7@gmail.com>
2025-12-18 08:41:26 +0545

    Rework on Tabs JS

--- Tabs/script.js
+++ Tabs/script.js
@@ -1,9 +1,7 @@
 let liElms = document.querySelectorAll("li");
 let contentBox = document.querySelector(".content-box");
-let firstBox = document.querySelector(".first");
-let secondBox = document.querySelector(".second");
-let thirdBox = document.querySelector(".third");
-let fourthBox = document.querySelector(".fourth");
+const boxes = document.querySelectorAll(".content-box>div");
+console.log(boxes);
 console.log(liElms);
 
 // for (let liElm of liElms) {
@@ -13,42 +11,22 @@ console.log(liElms);
 //   });
 // }
 
-document.addEventListener("DOMContentLoaded", () => {
-  secondBox.style.display = "none";
-  thirdBox.style.display = "none";
-  fourthBox.style.display = "none";
+boxes.forEach((box, index) => {
+  if (index === 0) {
+    box.style.display = "block";
+  } else {
+    box.style.display = "none";
+  }
 });
 
 liElms.forEach((li, index) => {
   li.addEventListener("click", () => {
-    if (index === 0) {
-      console.log("First Tab");
-      firstBox.style.display = "block";
-      secondBox.style.display = "none";
-      thirdBox.style.display = "none";
-      fourthBox.style.display = "none";
-    }
-    if (index === 1) {
-      console.log("Second Tab");
-      firstBox.style.display = "none";
-      thirdBox.style.display = "none";
-      fourthBox.style.display = "none";
-      secondBox.style.display = "block";
-    }
-    if (index === 2) {
-      console.log("Third Tab");
-      secondBox.style.display = "none";
-      firstBox.style.display = "none";
-      fourthBox.style.display = "none";
-      thirdBox.style.display = "block";
-    }
-
-    if (index === 3) {
-      console.log("Fourth Tab");
-      secondBox.style.display = "none";
-      thirdBox.style.display = "none";
-      firstBox.style.display = "none";
-      fourthBox.style.display = "block";
-    }
+    boxes.forEach((box, i) => {
+      if (index === i) {
+        box.style.display = "block";
+      } else {
+        box.style.display = "none";
+      }
+    });
   });
 });
