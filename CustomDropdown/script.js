4f08c08132dbbc8cb4ad6b80331da82fb2482949
Zane <subitbravepoudel7@gmail.com>
2025-12-23 08:10:19 +0545

    Made custom DropDown

---
+++ CustomDropdown/script.js
@@ -0,0 +1,25 @@
+const qnElm = document.querySelector(".qn");
+let textElm = document.querySelector(".text");
+const optDiv = document.querySelector(".options");
+const optElm = document.querySelectorAll(".options > div");
+// console.log(qnElm);
+
+qnElm.addEventListener("click", () => {
+  optDiv.classList.toggle("showOptions");
+});
+
+optElm.forEach((opt, index) => {
+  opt.addEventListener("click", () => {
+    document.querySelectorAll(".tick").forEach((t) => {
+      t.remove();
+    });
+    optDiv.classList.add("showOptions");
+    textElm.textContent = `${opt.textContent}`;
+
+    // opt.innerHTML += `<span> <i class="fa-solid fa-square-check"></i> </span>`;
+    const tick = document.createElement("span");
+    tick.className = "tick";
+    tick.innerHTML = `<i class="fa-solid fa-square-check">`;
+    opt.appendChild(tick);
+  });
+});
